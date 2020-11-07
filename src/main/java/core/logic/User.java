b3388803be21ae3a0d0d1f28a2ca8bc3e7225d4b
Nicolas <manuelnmatute@gmail.com>
2020-11-07 17:06:01 -0500

    User Class basic methods working.
    Methods like add,update & delete from database working.
    
    Also I add 3 new methods to manage user rols stuff:
    -getRol() as int
    -getRol() as String
    -createRols() to set custom rols.

--- src/main/java/core/logic/User.java
+++ src/main/java/core/logic/User.java
@@ -15,34 +15,52 @@
  */
 package core.logic;
 
+import core.db.DataBase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  * User (Employee) template.
  * @author NM4TT - https://github.com/NM4TT
  */
 public class User extends Person{
     
+    private String ID;
     private String username;
     private String password;
     /**
-     * This is the employee rol, for instance: Manager.
+     * This is the employee rol, an int value of 0 means <tt>none rol</tt> status.
      */
-    private String rol;
+    private int rol;
 
     /**
-     * Default constructor that set attributes to null, but rol sets to "Regular".
+     * Default constructor that set attributes to null, but rol sets to <tt>none rol</tt>.
      */
     public User(){
+        this.ID = null;
         this.name = null;
         this.lastname = null;
         this.username = null;
         this.email = null;
         this.celphone = null;
         this.password = null;
-        this.rol = "Regular";
+        this.rol = 0; //Zero is a none rol status.
     }
     
     /**
+     * Database table name of customers.
+     */
+    static final String TABLE = "employees";
+    
+    /**
      * Constructor with all the class attributes.
+     * @param id
      * @param name
      * @param lastname
      * @param email
@@ -51,7 +69,8 @@ public class User extends Person{
      * @param password
      * @param rol
      */
-    public User(String name, String lastname, String email, String celphone, String username, String password, String rol){
+    public User(String id, String name, String lastname, String email, String celphone, String username, String password, int rol){
+        this.ID = id;
         this.name = name;
         this.lastname = lastname;
         this.username = username;
@@ -62,51 +81,251 @@ public class User extends Person{
     }
     
     @Override
-    public void addToDatabase(){
-        
+    public boolean addToDatabase(){
+        boolean taskDone = false;
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;
+              
+            try {
+
+                if (cn != null) {
+                    pst = cn.prepareStatement("INSERT INTO " + TABLE + " VALUES (?,?,?,?,?,?,?,?)");
+
+                    pst.setString(1, this.getID());
+                    pst.setString(2, this.getName());
+                    pst.setString(3, this.getLastname());
+                    pst.setString(4, this.getEmail());
+                    pst.setString(5, this.getCelphone());
+                    pst.setString(6, this.getUsername());
+                    pst.setString(7, this.getPassword());
+                    pst.setInt(8, this.getRol());
+
+                    pst.executeUpdate();
+                    taskDone = true;
+                }
+                
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR ADDING USER", JOptionPane.ERROR_MESSAGE);
+            } finally {
+                DataBase.close(pst, cn);
+            }
+        return taskDone; 
+    }
+    
+    /**
+     * Method to update a user in database.
+     * @param newUser
+     * @return taskDone true or false.
+     */    
+    public boolean update(User newUser){
+        boolean taskDone = false;
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;      
+               
+            try {
+                    if (cn != null) {
+                        pst = cn.prepareStatement("UPDATE " + TABLE + " SET Employee_ID = ?, Employee_Name = ?, Employee_LastName = ?, Employee_Email = ?, Employee_Celphone = ?, Employee_User = ?, Employee_Password = ?, Employee_Position = ? WHERE Employee_ID = ?");             
+                        pst.setString(1, newUser.getID());
+                        pst.setString(2, newUser.getName());
+                        pst.setString(3, newUser.getLastname());
+                        pst.setString(4, newUser.getEmail());
+                        pst.setString(5, newUser.getCelphone());
+                        pst.setString(6, newUser.getUsername());
+                        pst.setString(7, newUser.getPassword());
+                        pst.setInt(8, newUser.getRol());
+                        pst.setString(9, this.getID());
+
+                        pst.executeUpdate();
+
+                        taskDone = true;
+                    }
+                    
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR UPDATING USER", JOptionPane.ERROR_MESSAGE);
+            } finally {
+               DataBase.close(pst, cn); 
+            }
+        return taskDone; 
     }
     
     @Override
-    public void update(Person newUser){
+    public boolean deleteFromDatabase(){
+        boolean taskDone = false;
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;
         
-    }
-    
-    @Override
-    public void deleteFromDatabase(){
-        
+            try {
+                if (cn != null) {
+                    pst = cn.prepareStatement("DELETE FROM " + TABLE + " WHERE Employee_ID = ?");
+                
+                    pst.setString(1, this.getID());
+
+                    pst.executeUpdate();
+                    taskDone = true;
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR DELETING USER", JOptionPane.ERROR_MESSAGE);
+            } finally {
+                DataBase.close(pst, cn);
+            }
+        return taskDone;   
     }
     
     /**
      * Method to search an user in database.
-     * @param username
+     * @param userID
      * @return User
      */
-    public static User searchOnDatabase(String username){
+    public static User searchOnDatabase(String userID){
         User user = new User();
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;
+        ResultSet rs = null;
         
-        return user;
-    }    
+        try {
+             if (cn != null) {
+                pst = cn.prepareStatement("SELECT * FROM " + TABLE + " WHERE Employee_ID = ?");
+                pst.setString(1, userID);
+                rs = pst.executeQuery();
+
+                while (rs.next()) {                
+                    user.setID(userID);
+                    user.setName(rs.getString("Employee_Name"));
+                    user.setLastname(rs.getString("Employee_LastName"));
+                    user.setEmail(rs.getString("Employee_Email"));
+                    user.setCelphone(rs.getString("Employee_Celphone"));
+                    user.setUsername(rs.getString("Employee_User"));
+                    user.setPassword(rs.getString("Employee_Password"));
+                    user.setRol(rs.getInt("Employee_Position"));
+                }                
+            }
+ 
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR SEARCHING USER", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            DataBase.close(rs, pst, cn);
+        }
+        
+        if (user.getID() != null) {
+            return user;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * This method adds custom rols to the DataBase in order to classify users
+     * <p>This method <b>can only be executed once</b> to set the rols.
+     * @param rols is the rol list as an array structure.
+     * @return <b>taskDone</b> as <tt>true</tt> or <tt>false</tt>
+     */
+    public static boolean createUserRols(String rols[]){
+        boolean taskDone = false;
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;
+        
+        //This is done in order to include a NONE ROL status know as ID 0.
+        List<String> ListOfRols = new ArrayList<>();
+        ListOfRols.add("NONE");
+        ListOfRols.addAll(Arrays.asList(rols));
+        
+        try {
+             if (cn != null) {
+                pst = cn.prepareStatement("INSERT INTO positions VALUES(?,?)");
+                
+                 for (int i = 0; i < ListOfRols.size(); i++) {
+                     pst.setInt(1, i); //from 1 and so on.
+                     pst.setString(2, ListOfRols.get(i));
+                     pst.execute();
+                 }
+                taskDone = true;
+            }
+ 
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR CREATING USER ROLS", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            DataBase.close(pst,cn);
+        }
+        
+        return taskDone;
+    }
     
-    
-    
-    
-    
-    
+    /**
+     * Method to know which ID rol an user has in the database. 
+     * <p>This method returns <tt>0</tt> if there is no ID with that name.
+     * @param rolName is the rol name to find the specific ID.
+     * @return ID of user's rol.
+     */
+    public static int getRol(String rolName){
+        int rol = 0;
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+             if (cn != null) {
+                pst = cn.prepareStatement("SELECT ID FROM positions WHERE Position_Name = ?");
+                pst.setString(1,rolName);
+                rs = pst.executeQuery();
+                
+                if(rs.next()){
+                    rol = rs.getInt("ID");
+                }
+                
+            }
+ 
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR GETTING ROL ID", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            DataBase.close(rs,pst,cn);
+        }        
+        return rol;
+    }
+
+    /**
+     * Method to know which rol name an user has in the database. 
+     * <p>This method returns <tt>null</tt> if there is no rol with that ID.
+     * @param rolID is the rol ID to find the specific name.
+     * @return User rol's name.
+     */
+    public static String getRol(int rolID){
+        String rol = null;
+        Connection cn = DataBase.connect(); 
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+             if (cn != null) {
+                pst = cn.prepareStatement("SELECT Position_Name FROM positions WHERE ID = ?");
+                pst.setInt(1,rolID);
+                rs = pst.executeQuery();
+                
+                if(rs.next()){
+                    rol = rs.getString("Position_Name");
+                }
+                
+            }
+ 
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage(),"ERROR GETTING ROL NAME", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            DataBase.close(rs,pst,cn);
+        }        
+        return rol;
+    }
     
     @Override
-    public void sendEmail(Person entity, String message) {
-        
+    public boolean sendEmail(String title, String message) {
+        return false;
     }    
     
     @Override
-    public void clean_Stored_Data(Person entity) {
+    public void clean_Stored_Data() {
         this.setCelphone(null);
         this.setEmail(null);
         this.setLastname(null);
         this.setName(null);
         this.setUsername(null);
         this.setPassword(null);
-        this.setRol(null);
+        this.setRol(0);
     }
 
     /**
@@ -140,14 +359,14 @@ public class User extends Person{
     /**
      * @return the rol
      */
-    public String getRol() {
+    public int getRol() {
         return rol;
     }
 
     /**
      * @param rol the rol to set
      */
-    public void setRol(String rol) {
+    public void setRol(int rol) {
         this.rol = rol;
     }
 
@@ -206,5 +425,19 @@ public class User extends Person{
     public void setCelphone(String celphone) {
         this.celphone = celphone;
     }    
+
+    /**
+     * @return the ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
     
 }
